3b47d41a43e26a1d9fb87d9c4f6d78f14a2afb03
Xopher <xpollard@jdmellberg.com>
2017-10-21 17:16:28 -0700

    remove cache;

--- cache/production/data_text_formatter_renderer.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1540163061
-79
-a:1:{s:5:"class";s:53:"s9e_renderer_c83235586a30524a39cb0cbcaf245afe39399228";}
--- cache/production/sql_e4a2e7bad854ef159c56c015e50a60ca.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1508634338
-SELECT * FROM phpbb_styles s WHERE s.style_id = 13
-293
-a:1:{i:0;a:8:{s:8:"style_id";s:2:"13";s:10:"style_name";s:5:"PBHoG";s:15:"style_copyright";s:28:"© PayBas 2015, @Sajaki 2017";s:12:"style_active";s:1:"1";s:10:"style_path";s:5:"pbhog";s:15:"bbcode_bitfield";s:4:"+Ng=";s:15:"style_parent_id";s:1:"1";s:17:"style_parent_tree";s:9:"prosilver";}}
